a4ddb77b9eb9188c74d20621ea95aa53abbf9d93
Esam Uddin <esam191@hotmail.com>
2022-02-03 07:50:46 -0500

    minor update

--- Project Milestone 1/Esam/Main.java
+++ Project Milestone 1/Esam/Main.java
@@ -2,9 +2,15 @@ public class Main{
 
 	public static void main(String[] args) {
     
-        	System.out.println("Hello World");
+		System.out.println("Hello World");
+		int c = 0;
+		try {
+			while(true){
+					Thread.sleep(2*1000);
+					System.out.println("I am still here! Iteration " + c++);
+			} 
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
-
-
-
